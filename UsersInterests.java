53cbe6fdd57b42ba658583e95738cbe56421a7b5
TaniaKaragianni <149497331+TaniaKaragianni@users.noreply.github.com>
2023-11-27 21:28:01 +0200

    Update UsersInterests.java

--- UsersInterests.java
+++ UsersInterests.java
@@ -10,13 +10,8 @@ public class UsersInterests {
     System.out.print("Σε ποια περιοχή θα επιθυμούσατε να δραστηριοποιηθείτε;");
     String location = scanner.next();
     System.out.print("Ποια κατηγορία προτιμάτε: 1.δράσεις δια ζώσης   2.διαδικτυακό εθελοντισμό ");
-    int c = scanner.nextInt();
-    String category;
-    if (c == 1){
-      category = "δράσεις δια ζώσης";
-    }else{
-      category = "διαδικτυακό εθελοντισμό";
-    }
+    int category = scanner.nextInt();
+    
     
 
 
