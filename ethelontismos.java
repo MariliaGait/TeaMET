46c827ee1719bedfb4d6373e8067df89d27a9586
MariliaGait <149596382+MariliaGait@users.noreply.github.com>
2023-11-29 19:53:57 +0200

    Update ethelontismos.java

--- ethelontismos.java
+++ ethelontismos.java
@@ -9,59 +9,67 @@ import java.nio.file.attribute.UserPrincipalLookupService;
 import java.util.Scanner;
 
 public class eTHELOntismos {
-  public static void main(String[] args) {
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Καλώς ήρθατε στον εΘΕΛΩντισμό! Είστε έτοιμοι για ένα νέο εθελοντικό ταξίδι;");
-      // δημιουργια κλασης για το προφιλ χρηστη
-      UsersProfile userProfile = UsersProfile.collectUserInformation();
-      // καλω την κλαση που περιλαμβανει το προφιλ του χρηστη και απο αυτη χρειαζομαι μονο ονομα και ηλικια
-      String userName = userProfile.getUsername();
-      int userAge = userProfile.getAge();
-      System.out.println("Ας ανακαλύψουμε τι σας ταιριάζει!");
-      // δημιουργια κλασης για τα ενδιαφεροντα του χρηστη 
 
+    public static String chatGPT(String prompt) {
+        String url = "https://chat.openai.com/";
+        String apiKey = "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";
+        String model = "gpt-3.5-turbo";
 
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Authorization", "Bearer " + apiKey);
+            connection.setRequestProperty("Content-Type", "application/json");
 
+            // The request body
+            String body = "{\"model\": \"" + model + "\", \"messages\": [{\"role\": \"user\", \"content\": \"" + prompt + "\"}]}";
+            connection.setDoOutput(true);
+            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+            writer.write(body);
+            writer.flush();
+            writer.close();
 
+            // Response from ChatGPT
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
 
+            StringBuffer response = new StringBuffer();
 
-
-
-        // Ask other relevant questions and collect user responses
-
-        // Step 2: Connect to GPT-3.5 for Suggestions
-        String apiKey = "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";
-        String apiUrl = "https://api.openai.com/v1/engines/davinci-codex/completions";
-        
-        // Construct the prompt based on user responses
-        String prompt = "Given the following information about " + userName + ", who is " + userAge + " years old, located in " + userLocation + ", and interested in " + userInterests + ", suggest suitable volunteering opportunities: ...";
-        String payload = "{ \"prompt\": \"" + prompt + "\", \"max_tokens\": 150 }";
-
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-            HttpPost httpPost = new HttpPost(apiUrl);
-            httpPost.addHeader("Content-Type", "application/json");
-            httpPost.addHeader("Authorization", "Bearer " + apiKey);
-            httpPost.setEntity(new StringEntity(payload));
-
-            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
-                HttpEntity entity = response.getEntity();
-                if (entity != null) {
-                    // Step 3: Display Suggestions to the User
-                    String suggestions = EntityUtils.toString(entity);
-                    System.out.println("Volunteering Suggestions:\n" + suggestions);
-                }
+            while ((line = br.readLine()) != null) {
+                response.append(line);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            br.close();
+
+            // calls the method to extract the message.
+            return extractMessageFromJSONResponse(response.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+    }
 
-        // Step 4: User Interaction and Refinement
-        // Allow the user to provide feedback or refine the suggestions
+    public static String extractMessageFromJSONResponse(String response) {
+        int start = response.indexOf("content") + 11;
 
-        // Step 5: Integration with Volunteering Platforms
-        // Optionally, integrate with real-time volunteering opportunities
+        int end = response.indexOf("\"", start);
 
-        // Close resources
-        scanner.close();
+        return response.substring(start, end);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Καλώς ήρθατε στον εΘΕΛΩντισμό! Είστε έτοιμοι για ένα νέο εθελοντικό ταξίδι;");
+        // δημιουργια κλασης για το προφιλ χρηστη
+        UsersProfile userProfile = UsersProfile.collectUserInformation();
+        // καλω την κλαση που περιλαμβανει το προφιλ του χρηστη και απο αυτη χρειαζομαι μονο ονομα και ;ηλικια;
+        String userName = userProfile.getUsername();
+        //int userAge = userProfile.getAge();
+        System.out.println("Ας ανακαλύψουμε τι σας ταιριάζει!");
+        //kaloume thn usersinterests
+        UsersInterests usersint = new UsersInterests();
+        String result = usersint.return_result();
+        System.out.println(chatGPT("Ο χρήστης με το όνομα" + userName + "ενδιαφέρεται να λάβει μέρος σε μια εθελοτνική δράση. Ενδιαφέρεται για :" + result + " Πρότεινέ του την κατάλληλη εθελοντική εργασία"));
     }
 }
